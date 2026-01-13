1cd83eab3bfe9737c3bcb32425104cfad950c098
Harsh Narwariya <h.narwariya@thesynapses.com>
2026-01-13 14:15:24 +0530

    Chats Store Updated

--- db/datacontroller.py
+++ db/datacontroller.py
@@ -16,21 +16,21 @@ class Chat:
         self.client = client
 
     def delete_chat(self, userId, chatId):
-        chat_ref = self.client.collection("users").document(userId).collection("chats").document(chatId)
+        chat_ref = self.client.collection("chats").document(chatId)
         self.client.recursive_delete(chat_ref)
 
         chat_ref = self.client.collection("users").document(userId).collection("drafts").document(chatId)
         self.client.recursive_delete(chat_ref)
 
     def rename_chat(self, userId, chatId, new_title):
-        chat_ref = self.client.collection("users").document(userId).collection("chats").document(chatId)
+        chat_ref = self.client.collection("chats").document(chatId)
         chat_ref.update({
             "title": new_title,
             # "updated_at": firestore.SERVER_TIMESTAMP
         })
 
     def get_messages(self, userId, chatId, limit=CONFIG.GEMINI_MESSAGE_LIMIT, should_yeild=False):
-        messages_ref = self.client.collection("users").document(userId).collection("chats").document(chatId).collection("messages").order_by("created_at", direction=firestore.Query.ASCENDING).limit(limit)
+        messages_ref = self.client.collection("chats").document(chatId).collection("messages").order_by("created_at", direction=firestore.Query.ASCENDING).limit(limit)
         if should_yeild:
             messages_docs = messages_ref.stream()
             for msg in messages_docs:
